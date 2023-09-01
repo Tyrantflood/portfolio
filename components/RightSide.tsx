886dcdae46fc4d8f3067afbf0db2de5e77dac1ce
Tyrantflood <princeobi124@gmail.com>
2023-09-01 11:34:00 +0100

    final email change

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function RightSide() {
   return (
     <div className=" w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
-      <p className=" text-sm rotate-90 w-80 tracking-wide  hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300">
+      <p className=" text-sm rotate-90 w-80 tracking-wide  hover:text-textGreen  hover:translate-y-2 transition-all duration-300">
         obiprince060@gmail.com{" "}
       </p>
       <div className="w-[2px] h-32 bg-textDark inline-flex"></div>
