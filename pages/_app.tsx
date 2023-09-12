a487476578876d61b2929ed3c7bbedcb8dee3c3d
Tyrantflood <princeobi124@gmail.com>
2023-09-12 13:12:45 +0100

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LNCMR69Y1X"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LNCMR69Y1X', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
